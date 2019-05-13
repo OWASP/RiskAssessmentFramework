ff9dc287de7e99e2aa4cca4333697f69d60fc4a8
azharanees <azharanees1@gmail.com>
2019-05-13 19:58:33 +0530

    fixed tslint errors

--- gsoc_dashboard/App/dashboard/src/app/material.module.ts
+++ gsoc_dashboard/App/dashboard/src/app/material.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from "@angular/core";
-import {MatButtonModule, MatIconModule} from '@angular/material/'
-import {MatCardModule,MatListModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSidenavModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+import {MatButtonModule, MatIconModule} from '@angular/material/';
+import {MatCardModule, MatListModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSidenavModule} from '@angular/material';
 import { AlertModule } from 'ngx-bootstrap';
 
 
@@ -9,9 +9,15 @@ import { AlertModule } from 'ngx-bootstrap';
 
 
 @NgModule({
-    imports:[MatButtonModule,MatListModule,MatToolbarModule,MatSidenavModule, MatIconModule,MatFormFieldModule, MatCardModule, MatInputModule,AlertModule ],
-    exports:[MatButtonModule,MatListModule,MatToolbarModule,MatSidenavModule, MatIconModule, MatFormFieldModule, MatCardModule, MatInputModule,AlertModule ]
+    imports: [MatButtonModule, MatListModule,
+       MatToolbarModule, MatSidenavModule,
+       MatIconModule, MatFormFieldModule,
+        MatCardModule, MatInputModule, AlertModule ],
+    exports: [MatButtonModule, MatListModule,
+       MatToolbarModule, MatSidenavModule,
+       MatIconModule, MatFormFieldModule,
+       MatCardModule, MatInputModule, AlertModule ]
 
 })
 
-export class MaterialModule{}
+export class MaterialModule {}
