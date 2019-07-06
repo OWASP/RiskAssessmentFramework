cdd7c7d204ea8ec8e98ff724e92e06cd6ca92d19
azharanees <azharanees1@gmail.com>
2019-07-06 15:10:13 +0530

    added the drag and drop upload and url upload feature

--- gui/src/app/view/tools/top10/top10.component.ts
+++ gui/src/app/view/tools/top10/top10.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import '@vaadin/vaadin-upload';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-top10',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top10.component.css']
 })
 export class Top10Component implements OnInit {
+   reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.reg)
+,
+  ]);
   constructor() { }
 
   ngOnInit() {
